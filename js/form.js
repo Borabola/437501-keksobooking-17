3ba87b6ca84ea15d9dfeeb50ef27bc2e7db51806
Olya <osokolova1@gmail.com>
2019-07-07 14:33:19 +0300

    Модули

---
+++ js/form.js
@@ -0,0 +1,84 @@
+'use strict';
+(function () {
+  var mapFilter = document.querySelector('.map__filters-container');
+  var mapFilterFieldsetList = mapFilter.querySelectorAll('fieldset');
+  var adForm = document.querySelector('.ad-form');
+  var adFormFieldsetList = adForm.querySelectorAll('fieldset');
+
+  var typeOfHousing = adForm.querySelector('#type');
+
+  var checkInTime = document.querySelector('#timein');
+  var checkOutTime = document.querySelector('#timeout');
+
+  /**
+   * Функция берет координаты mainPinX и mainPinY указателя пина и записывает их в строку адреса. В неактивном режиме круглый пин с координатами mainPinX, mainPinYInitial
+   * @param {boolean} isInitial после открытия страницы isInitial= true, после активации isInitial= false
+   */
+  window.fillAddress = function (isInitial) {
+    var adFormAddress = adForm.querySelector('#address');
+    var pinLocations = window.getMainPinLocation();
+    if (isInitial) {
+      var addressLine = pinLocations.mainPinX + ', ' + pinLocations.mainPinYInitial;
+    } else {
+      addressLine = pinLocations.mainPinX + ', ' + pinLocations.mainPinY;
+    }
+    adFormAddress.value = addressLine;
+  };
+
+  /**
+   * Функция дабавляет атрибут disabled к элементам
+   * @param {HTMLElement[]} elementList
+   */
+  function deactivateElements(elementList) {
+    for (var i = 0; i < elementList.length; i++) {
+      elementList[i].disabled = true;
+    }
+  }
+
+  /**
+   * Функция убирает атрибут disabled к элементам
+   * @param {HTMLElement[]} elementList
+   */
+  function activateElements(elementList) {
+    for (var i = 0; i < elementList.length; i++) {
+      elementList[i].disabled = false;
+    }
+  }
+
+  /* Функция переводит страницу в активное состояние */
+  window.activatePage = function () {
+    activateElements(adFormFieldsetList);
+    activateElements(mapFilterFieldsetList);
+    adForm.classList.remove('ad-form--disabled');
+    window.map.classList.remove('map--faded');
+  };
+
+  /* Функция переводит страницу в неактивное состояние*/
+  window.deactivatePage = function () {
+    deactivateElements(adFormFieldsetList);
+    deactivateElements(mapFilterFieldsetList);
+    window.fillAddress(true);
+  };
+
+  function onTypeInputChange() {
+    var minPrice = {
+      bungalo: 0,
+      flat: 1000,
+      house: 5000,
+      palace: 10000
+    };
+    var inputPrice = adForm.querySelector('#price');
+    inputPrice.min = inputPrice.placeholder = minPrice[typeOfHousing.value];
+  }
+
+  function onTimeInputChange() {
+    checkOutTime.value = checkInTime.value;
+  }
+
+  function onTimeOutInputChange() {
+    checkInTime.value = checkOutTime.value;
+  }
+  typeOfHousing.addEventListener('change', onTypeInputChange);
+  checkInTime.addEventListener('change', onTimeInputChange);
+  checkOutTime.addEventListener('change', onTimeOutInputChange);
+})();
