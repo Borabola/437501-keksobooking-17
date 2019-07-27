8c6c11985be6980ec12ad88b128a4147e6a09cdb
Olya <osokolova1@gmail.com>
2019-07-28 00:09:44 +0300

    Добавлен фильтр по стоимости жилья

--- js/filters.js
+++ js/filters.js
@@ -1,24 +1,52 @@
 'use strict';
 (function () {
   var mapFilters = document.querySelector('.map__filters');
-  window.housingTypeFilter = mapFilters.querySelector('#housing-type');
+  var housingTypeFilter = mapFilters.querySelector('#housing-type');
   var housingPrice = mapFilters.querySelector('#housing-price');
   var housingRoom = mapFilters.querySelector('#housing-rooms');
   var housingGuest = mapFilters.querySelector('#housing-guests');
   var housingFeatures = mapFilters.querySelectorAll('.map__checkbox');
+  var anyValue = 'any';
 
-  /**
-   * Функция фильтрования по типу жилья
-   * @type {function(): void}
-   */
-  window.onTypeChange = window.debounce(function () {
-    if (window.housingTypeFilter.value === 'any') {
-      window.filteredAds = window.ads;
-    } else {
-      window.filteredAds = window.ads.filter(function (ad) {
-        return ad.offer.type === window.housingTypeFilter.options[window.housingTypeFilter.selectedIndex].value;
-      });
+  var housingPriceMap = {
+    'low': {
+      min: 0,
+      max: 1000
+    },
+
+    'middle': {
+      min: 10000,
+      max: 50000
+    },
+
+    'high': 50000
+  };
+
+  var checkHousingTypeChange = function (ad) {
+    if (housingTypeFilter.value === anyValue) {
+      return true;
     }
+    return ad.offer.type === housingTypeFilter.options[housingTypeFilter.selectedIndex].value;
+  };
+
+  var checkPriceChange = function (ad) {
+    if (housingPrice.value === anyValue) {
+      return true;
+    } else if (housingPrice.value === 'high') {
+      return ad.offer.price >= housingPriceMap[housingPrice.value];
+    }
+    return ad.offer.price >= housingPriceMap[housingPrice.value].min && ad.offer.price < housingPriceMap[housingPrice.value].max;
+  };
+
+  function filterAd() {
+    window.filteredAds = window.ads.filter(function (ad) {
+      return checkHousingTypeChange(ad) &&
+        checkPriceChange(ad);
+    });
+  }
+
+  function onFiltersChange() {
+    filterAd();
     if (window.filteredAds.length === 0) {
       window.clearPins();
       window.isRenderPinsCalled = true;
@@ -26,15 +54,14 @@
       window.rerenderAds(window.filteredAds);
     }
     window.closeCard();
-  });
+  }
 
-  window.housingTypeFilter.addEventListener('change', window.onTypeChange);
-
+  mapFilters.addEventListener('change', onFiltersChange);
   window.resetFilters = function () {
-    window.housingTypeFilter.value = 'any';
-    housingPrice.value = 'any';
-    housingRoom.value = 'any';
-    housingGuest.value = 'any';
+    housingTypeFilter.value = anyValue;
+    housingPrice.value = anyValue;
+    housingRoom.value = anyValue;
+    housingGuest.value = anyValue;
     for (var i = 0; i < housingFeatures.length; i++) {
       housingFeatures[i].checked = false;
     }
